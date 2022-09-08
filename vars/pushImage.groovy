c31ef16a88c618d92305b68a9ce427da98fb8ecd
Mohit-dev5 <62936886+Mohit-dev5@users.noreply.github.com>
2022-09-08 10:31:16 +0530

    Create pushImage.groovy

---
+++ vars/pushImage.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+def call(){
+    echo "Pushing the Build Image into the ACR repo through shared library concept"
+        .. Use Below Comments when using Dockerhub
+        //withCredentials([usernamePassword(credentialsId: 'ACR', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        //sh 'docker build -t advisingbank/demo-app .'
+        //sh "echo $PASS | docker login -u $USER --password-stdin"
+        //sh 'docker push nanajanashia/demo-app:jma-2.0'
+      docker.withRegistry( "http://${registryUrl}", registryCredential ) {
+      dockerImage.push()    
+}
