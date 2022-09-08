e8aad0f7f9dbd0493e039f482f911c16a6a966e8
Mohit-dev5 <62936886+Mohit-dev5@users.noreply.github.com>
2022-09-08 12:49:08 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,4 +3,7 @@
 def call(){
     echo "building the docker image through Shared Library vars/groovy file"
     dockerImage = docker.build registryName + ":$BUILD_NUMBER"
+    echo "Pushing the Build Image into the ACR repo through shared library concept"
+    docker.withRegistry( "http://${registryUrl}", registryCredential ) {
+    dockerImage.push()    
 }
