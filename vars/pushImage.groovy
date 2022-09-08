9b3c36e0e9a2a6cd39730de4d11790fe566ba2ac
Mohit-dev5 <62936886+Mohit-dev5@users.noreply.github.com>
2022-09-08 12:46:09 +0530

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -4,6 +4,7 @@ def call(){
     echo "Pushing the Build Image into the ACR repo through shared library concept"
       docker.withRegistry( "http://${registryUrl}", registryCredential ) {
       dockerImage.push()    
+    }
 }
     
 return this
