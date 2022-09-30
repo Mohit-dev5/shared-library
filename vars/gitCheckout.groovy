3b5d55953254df62997f0c0c39844a34c306f6a4
Mohit-dev5 <62936886+Mohit-dev5@users.noreply.github.com>
2022-09-30 14:37:42 +0530

    Groovy file for checkout of code from branch

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+     def call(Map stageParams) {
+
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name:  stageParams.branch ]],
+            userRemoteConfigs: [[ url: stageParams.url ]]
+        ])
+    }
+
+return this
